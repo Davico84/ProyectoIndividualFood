7773b8bae25ef4033f03c60a1f55f83a32dd113d
Davico84 <davicova84@gmail.com>
2023-02-23 17:57:16 -0500

    first commit

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -24,9 +24,6 @@ module.exports = (sequelize) => {
     pasoAPaso:{
       type:DataTypes.STRING
     },
-    // image:{
-    //   type:DataTypes.STRING,
-    // },
     create:{
       type: DataTypes.BOOLEAN,
       defaultValue:true
